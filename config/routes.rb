aa86dd6c7e82a645f286a5271125be59a7d81842
samgirardeau <sam.girardeau@gmail.com>
2020-08-11 12:33:11 +0200

    new silk routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #afficher tasks
+  get '/tasks', to: 'tasks#index'
+  #afficher task
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #Créer task
+  get '/tasks/new', to: 'tasks#nex'
+  post '/tasks', to: 'tasks#create'
+  #Update task
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  #Delete task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
